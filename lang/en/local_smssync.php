075bebbb0bbcfdb6f4adf2b8157df030b812ab0f
vtos <potenkov@gmail.com>
2022-10-07 12:23:31 +0200

    Added checking for internal constant in the lang file.

--- lang/en/local_smssync.php
+++ lang/en/local_smssync.php
@@ -22,4 +22,6 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+defined('MOODLE_INTERNAL') || die();
+
 $string['pluginname'] = 'SMS (Student Management System) Integration';
